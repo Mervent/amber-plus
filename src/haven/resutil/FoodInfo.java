56631d8543b6a472dac714594ddc1a4283a1b662
mervent <lamkaslim@gmail.com>
2020-08-15 21:31:43 +0300

    Improve FoodInfo

--- src/haven/resutil/FoodInfo.java
+++ src/haven/resutil/FoodInfo.java
@@ -91,6 +91,10 @@ public class FoodInfo extends ItemInfo.Tip {
 
         Collection<BufferedImage> imgs = new LinkedList<BufferedImage>();
         imgs.add(base);
+        double totalFeps = 0;
+        for (int i = 0; i < evs.length; i++) {
+            totalFeps += evs[i].a;
+        }
         for (int i = 0; i < evs.length; i++) {
             Color col = Utils.blendcol(evs[i].ev.col, Color.WHITE, 0.5);
             String str;
@@ -100,11 +104,17 @@ public class FoodInfo extends ItemInfo.Tip {
                         col.getGreen(), col.getBlue(), Utils.odformat2(evs[i].a, 2), col.getRed(), col.getGreen(),
                         col.getBlue(), q != null ? basefepfmt.format(evs[i].a / Math.sqrt(q.q / 10)) : "???");
             } else {
-                str = String.format("%s: $col[%d,%d,%d]{%s}", evs[i].ev.nm, col.getRed(), col.getGreen(), col.getBlue(),
-                        Utils.odformat2(evs[i].a, 2));
+                str = String.format("%s: $col[%d,%d,%d]{%s (%s)}", evs[i].ev.nm, col.getRed(), col.getGreen(),
+                        col.getBlue(), Utils.odformat2(evs[i].a, 2),
+                        Utils.odformat2(evs[i].a / (totalFeps / 100.0), 2) + "%");
             }
             imgs.add(catimgsh(5, evs[i].img, RichText.render(str, 0).img));
         }
+        new Color(0, 0, 0);
+        imgs.add(RichText.render(
+                String.format("Total FEP: $col[%d,%d,%d]{%s}, FEP/Hunger: $col[%d,%d,%d]{%s}", 0, 180, 0,
+                        Utils.odformat2(totalFeps, 2), 0, 180, 0, Utils.odformat2(totalFeps / (glut * 100), 2)),
+                0).img);
         for (int i = 0; i < efs.length; i++) {
             BufferedImage efi = ItemInfo.longtip(efs[i].info);
             if (efs[i].p != 1)
